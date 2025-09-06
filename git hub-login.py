ef5055a5d16ddb78cf6dd6f9bfc0ae18520c17fe
NimaXploit <nimawolf121@gmail.com>
2025-09-06 14:06:57 +0330

    Update git hub-login.py

--- git hub-login.py
+++ git hub-login.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://github.com/{}'
-username = 'nima00723'
+username = ''
 
 session = requests.Session()
 r = session.get(url.format('login'))
@@ -13,10 +13,11 @@ for form in content.find_all('form'):
     for inp in form.select('input[type=hidden]'):
         data[inp.get('name')] = inp.get('value')
 
-data.update({'login':'nimawolf121@gmail.com', 'password':'0918750Nima'})
+data.update({'login':'', 'password':''})
 
 r = session.post(url.format('session'), data=data)
 r = session.get(url.format(username))
 content = BeautifulSoup(r.text, 'html.parser')
 user_info = content.find(class_='vcard-details')
 print(user_info.text)
+
